b60882906aa38a88de43cd540d5994157bea83f4
iammmmmmm <2946649116@qq.com>
2023-12-04 11:26:57 +0800

    新建jfx的文件夹

---
+++ src/main/java/jfx/jfx/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module codeexamples.jfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens codeexamples.jfx to javafx.fxml;
+    exports codeexamples.jfx;
+}
